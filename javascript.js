b561aca22f705e66d857b1f6d8bf20a3d544ff93
Howl Kimura <howlkimuradev@gmail.com>
2023-05-06 18:13:52 +0300

    Add functionality to DEL button and period

--- javascript.js
+++ javascript.js
@@ -8,8 +8,6 @@ function operate(num1, num2, operator) {
       return num1 * num2;
     case '/':
       return num1 / num2;
-    case '%':
-      return num1 % num2;
   }
 }
 
@@ -19,18 +17,23 @@ function main() {
   let operator = "";
 
   const display = document.querySelector(".display p");
+  const resultDisplay = document.querySelector("p .result");
   const buttons = document.querySelectorAll("button");
 
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
 
       const buttonText = button.textContent;
+      if (!buttonText) return;
 
       if (buttonText === 'AC') {
+
         display.textContent = '';
         num1 = undefined;
         num2 = undefined;
         operator = "";
+      } else if (buttonText === 'DEL') {
+          display.textContent = display.textContent.slice(0,-1);
       } else {
 
         if (display.textContent === operator) {
@@ -38,20 +41,23 @@ function main() {
         }
 
         if(buttonText === '=') {
+          num2 = +display.textContent;
           let result = operate(num1, num2, operator);
           display.textContent = result;
           num1 = result;
         } else {
           if (Number.isInteger(+buttonText)) {
             display.textContent += buttonText;
-            if (num1 === undefined) {
-              num1 = +display.textContent;
-            } else {
-              num2 = +display.textContent;
-            }
           } else {
-            operator = buttonText;
-            display.textContent = operator;
+            if (buttonText === '.') {
+              display.textContent += '.';
+            } else {
+              operator = buttonText;
+              if (num1 === undefined) {
+                num1 = +display.textContent;
+              } 
+              display.textContent = operator;
+            }
           }
         }
       }
